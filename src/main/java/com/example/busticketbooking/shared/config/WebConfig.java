84b27edd75b623fb1fe16ce9dd66f6a0b2ba3966
Markéta Kubíková <kubikovamarket@gmail.com>
2025-07-16 21:29:45 +0200

    added web config

---
+++ src/main/java/com/example/busticketbooking/shared/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.example.busticketbooking.shared.config;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Value("${frontend.base-url}")
+    private String frontendBaseUrl;
+    @Value("${frontend.max-age}")
+    private int frontendMaxAge;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(frontendBaseUrl)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(frontendMaxAge);
+            }
+        };
+    }
+}
